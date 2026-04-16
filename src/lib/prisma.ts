2806b2b474b8387ec98df856d50d5ea9c7fe400e
Mofe Odujirin <thebaggieboy@protonmail.com>
2026-04-15 20:01:06 -0400

    jbdsubfiubin

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { Pool, neonConfig } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = globalThis as unknown as { prisma_new: PrismaClient };
 
 function createPrismaClient() {
-  return new PrismaClient();
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma_new ?? createPrismaClient();
